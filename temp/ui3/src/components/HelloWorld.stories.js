6a19411f74565299fbf6ce7dda913c46d000997f
Jiadong Chen <jiadchen@redhat.com>
2023-01-26 13:35:45 +0900

    add helloworld story

---
+++ temp/ui3/src/components/HelloWorld.stories.js
@@ -0,0 +1,31 @@
+import { rest } from "msw";
+import HelloWorld from "./HelloWorld.vue";
+
+export default {
+  title: "Component/HelloWorld",
+  component: HelloWorld,
+};
+
+const Template = (args, { argTypes }) => ({
+  components: { HelloWorld },
+  props: Object.keys(argTypes),
+  template: '<HelloWorld v-bind="$props" />',
+});
+
+export const TestMessage = Template.bind({});
+TestMessage.args = {
+  msg: "Test Message",
+};
+TestMessage.parameters = {
+  msw: {
+    handlers: [
+      rest.get("/user", (req, res, ctx) => {
+        return res(
+          ctx.json({
+            result: "this is a test.",
+          })
+        );
+      }),
+    ],
+  },
+};
