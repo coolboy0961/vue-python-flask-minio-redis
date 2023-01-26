8ee5578d2158db6f42a4c7afe9ee12c594bc74dc
Jiadong Chen <jiadchen@redhat.com>
2023-01-26 12:09:11 +0900

    storybook msw addon追加

--- temp/ui-vue-cli-vue2-javascript/.storybook/main.js
+++ temp/ui-vue-cli-vue2-javascript/.storybook/main.js
@@ -1,15 +1,21 @@
+const path = require("path");
+
 module.exports = {
-  "stories": [
-    "../src/**/*.stories.mdx",
-    "../src/**/*.stories.@(js|jsx|ts|tsx)"
-  ],
-  "addons": [
+  stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
-    "@storybook/addon-interactions"
+    "@storybook/addon-interactions",
   ],
-  "framework": "@storybook/vue",
-  "core": {
-    "builder": "@storybook/builder-webpack5"
-  }
-}
+  framework: "@storybook/vue",
+  core: {
+    builder: "@storybook/builder-webpack5",
+  },
+  staticDirs: ["../public"],
+  webpackFinal: async (config, { configType }) => {
+    // Allow the use of Vue src alias in components.
+    config.resolve.alias["@"] = path.resolve(__dirname, "../src");
+
+    return config;
+  },
+};
