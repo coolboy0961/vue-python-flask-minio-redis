d4fdf5148027525e505adb8d062675cb42a6c987
Jiadong Chen <jiadchen@redhat.com>
2023-01-26 20:19:19 +0900

    api 不要なテストファイル削除

--- bff/tests/api-test/hello_test.py
+++ bff/tests/api-test/hello_test.py
@@ -1,16 +1,18 @@
-from app import create_app
+import requests
+
+base_url = "http://127.0.0.1:5000"
 
 
-def test_normal(client):
+def test_normal():
     """
     / にアクセスして正常なレスポンスを返すこと
     """
     # Arrange
-    expected = b"Hello, World!"
+    expected = "Hello, World!"
 
     # Act
-    response = client.get("/")
+    response = requests.get(url=base_url + "/")
 
     # Assert
     assert response.status_code == 200
-    assert response.data == expected
+    assert response.text == expected
--- bff/tests/api-test/user_test.py
+++ bff/tests/api-test/user_test.py
@@ -1,35 +1,36 @@
+import requests
 import json
 
-from app import create_app
+base_url="http://127.0.0.1:5000"
 
 
-def test_normal(client):
+def test_normal():
     """
     /users にアクセスして正常なレスポンスを返すこと
     """
     # Arrange
-    expected = bytes(json.dumps([
+    expected = json.dumps([
         {
-            'id': 1,
-            'name': 'TestUser1',
-            'email': 'testuser1@test.com'
+            "id": 1,
+            "name": "TestUser1",
+            "email": "testuser1@test.com"
         },
         {
-            'id': 2,
-            'name': 'TestUser2',
-            'email': 'testuser2@test.com'
+            "id": 2,
+            "name": "TestUser2",
+            "email": "testuser2@test.com"
         },
         {
-            'id': 3,
-            'name': 'TestUser3',
-            'email': 'testuser3@test.com'
+            "id": 3,
+            "name": "TestUser3",
+            "email": "testuser3@test.com"
         }
-    ]), "utf-8")
+    ])
 
     # Act
-    actual_response = client.get("/users")
+    actual_response = requests.get(url=base_url + "/users")
 
     # Assert
     assert actual_response.status_code == 200
-    print(actual_response.data)
-    assert actual_response.data == expected
+    print(actual_response.text)
+    assert actual_response.text == expected
