4f4865b8127f1c3907c7f02f0657c7a80bf5541f
Jiadong Chen <jiadchen@redhat.com>
2023-01-27 18:39:36 +0900

    テスト後ゴミファイルを削除できるようにした

--- polling-batch/tests/unit-test/test_MinioClient/test_MinioClient.py
+++ polling-batch/tests/unit-test/test_MinioClient/test_MinioClient.py
@@ -1,5 +1,6 @@
 
 import os
+import glob
 from src.infrastructure.MinioClient import MinioClient
 
 class Test:
@@ -7,6 +8,7 @@ class Test:
         """setup any state tied to the execution of the given method in a
         class.  setup_method is invoked for every test method of a class.
         """
+        print("setup")
         minio_client = MinioClient()
         minio_client.remove_all_objects("users")
 
@@ -15,8 +17,16 @@ class Test:
         """teardown any state that was previously setup with a setup_method
         call.
         """
+        print("tear down")
         minio_client = MinioClient()
         minio_client.remove_all_objects("users")
+        current_path = os.path.dirname(os.path.abspath(__file__))
+        download_path = os.path.normpath(os.path.join(current_path, 'download'))
+
+        pattern = download_path + "/*.jpg"
+        files = glob.glob(pattern)
+        for file in files:
+            os.remove(file)
 
     def test_ファイルを正しくアップロードできること(self, image_diff):
         # Arrange
@@ -32,5 +42,6 @@ class Test:
         target.download(
             'users', download_path, 'testfile.jpg')
         actual = download_path + '/testfile.jpg'
+
         # Assert
         assert image_diff(expected, actual)
