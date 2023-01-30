7ad0d4fda7e288b54ad8cb3fdf58e7a6db946fb9
Jiadong Chen <jiadchen@redhat.com>
2023-01-30 11:41:42 +0900

    cypressのレポートを出力できるようにした

--- e2e/cypress.config.js
+++ e2e/cypress.config.js
@@ -1,9 +1,14 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  reporter: "cypress-mochawesome-reporter", // https://github.com/LironEr/cypress-mochawesome-reporter
+  reporterOptions: {
+    embeddedScreenshots: true,
+  },
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
+      require("cypress-mochawesome-reporter/plugin")(on);
     },
   },
 });
