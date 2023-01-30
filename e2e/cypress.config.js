b907f25b135e5bb1fbbf42f62fef299a594b3ece
Jiadong Chen <jiadchen@redhat.com>
2023-01-30 14:10:37 +0900

    cypressの環境変数を切り替えられるようにした

--- e2e/cypress.config.js
+++ e2e/cypress.config.js
@@ -1,4 +1,20 @@
-const { defineConfig } = require("cypress");
+const { Cypress, defineConfig } = require("cypress");
+
+let env;
+switch (process.env.STAGE) {
+  case "local-confirm":
+    env = {
+      stage: process.env.STAGE,
+      baseUrl: "http://localhost:8080",
+    };
+    break;
+  case "dev":
+    env = {
+      stage: process.env.STAGE,
+      baseUrl: "http://dev:8080",
+    };
+    break;
+}
 
 module.exports = defineConfig({
   reporter: "cypress-mochawesome-reporter", // https://github.com/LironEr/cypress-mochawesome-reporter
@@ -11,4 +27,5 @@ module.exports = defineConfig({
       require("cypress-mochawesome-reporter/plugin")(on);
     },
   },
+  env,
 });
