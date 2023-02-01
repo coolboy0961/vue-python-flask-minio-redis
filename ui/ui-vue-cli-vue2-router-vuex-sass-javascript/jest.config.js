011410c0d8073bcbec8d715a2a50fe493a21257f
Jiadong Chen <jiadchen@redhat.com>
2023-02-01 10:42:19 +0900

    vue projectのsonarqube連携を追加した

--- ui/ui-vue-cli-vue2-router-vuex-sass-javascript/jest.config.js
+++ ui/ui-vue-cli-vue2-router-vuex-sass-javascript/jest.config.js
@@ -3,4 +3,9 @@ module.exports = {
   preset: "@vue/cli-plugin-unit-jest",
   transformIgnorePatterns: ["/node_modules/(?!axios)"],
   setupFiles: ["<rootDir>/tests/setup.js"],
+  testResultsProcessor: "jest-sonar-reporter",
+  collectCoverageFrom: [
+    "src/**/*.{js,jsx,ts,tsx,vue}",
+    "!<rootDir>/node_modules/",
+  ],
 };
