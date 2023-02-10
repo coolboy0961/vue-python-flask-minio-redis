f101ecca5f74be9ca063e4aa076ebaecc13c47f3
jiadchen <jiadchen@redhat.com>
2023-02-10 19:09:17 +0900

    apiの実装方法を修正

--- ui/ui-vue-cli-vue2-router-vuex-sass-javascript/src/utils/apis/index.js
+++ ui/ui-vue-cli-vue2-router-vuex-sass-javascript/src/utils/apis/index.js
@@ -1,18 +1,10 @@
 import { axiosInstance } from "./axios-instance";
-// export const api = {
-//   getUser: async () => {
-//     const result = await axiosInstance.get("/user").data;
-//     console.log("result is " + result);
-//     return result;
-//   },
-// };
-
-class Api {
-  async getUser() {
+export const api = {
+  getUser: async () => {
     const result = (await axiosInstance.get("/user")).data;
     console.log("result is " + result);
     return result;
-  }
-}
+  },
+};
 
-export default new Api();
+export default api;
