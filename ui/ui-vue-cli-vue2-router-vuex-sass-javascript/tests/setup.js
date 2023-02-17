d540b1c39c4c9147e76a121a9f52dbef58f15d41
jiadchen <jiadchen@redhat.com>
2023-02-17 21:42:57 +0900

    fix ut

--- ui/ui-vue-cli-vue2-router-vuex-sass-javascript/tests/setup.js
+++ ui/ui-vue-cli-vue2-router-vuex-sass-javascript/tests/setup.js
@@ -1,5 +1,7 @@
 // fix [Vue warn]: Unknown custom element
 import Vue from "vue";
 import Vuetify from "vuetify";
+import api from "@/utils/apis";
 Vue.config.productionTip = false;
 Vue.use(Vuetify);
+Vue.prototype.$api = api;
