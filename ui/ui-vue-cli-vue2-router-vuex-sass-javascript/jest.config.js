abe9efeeed4fb49c6d284217d98d9cb0dc838113
jiadchen <jiadchen@redhat.com>
2023-02-21 19:08:23 +0900

    vuetifyでVistualテストを実行できるようにした

--- ui/ui-vue-cli-vue2-router-vuex-sass-javascript/jest.config.js
+++ ui/ui-vue-cli-vue2-router-vuex-sass-javascript/jest.config.js
@@ -1,11 +1,14 @@
 require("./load-env");
 module.exports = {
   preset: "@vue/cli-plugin-unit-jest",
-  transformIgnorePatterns: ["/node_modules/(?!axios)"],
+  transformIgnorePatterns: ["/node_modules/(?!axios|vuetify)"],
   setupFiles: ["<rootDir>/tests/setup.js"],
   testResultsProcessor: "jest-sonar-reporter",
   collectCoverageFrom: [
     "src/**/*.{js,jsx,ts,tsx,vue}",
     "!<rootDir>/node_modules/",
   ],
+  moduleNameMapper: {
+    "\\.(css|scss)$": "<rootDir>/node_modules/jest-css-modules",
+  },
 };
